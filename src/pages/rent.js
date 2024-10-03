2af8b567946c24cbd635eca2f76ae04109522d46
Devesh Poojary <119301727+Deveshpoojary@users.noreply.github.com>
2024-10-03 12:42:51 +0530

    Update rent.js

--- src/pages/rent.js
+++ src/pages/rent.js
@@ -14,7 +14,7 @@ const UpdatePrices = () => {
         }
 
         try {
-            const response = await fetch('http://localhost:3001/api/updatePrices', {
+            const response = await fetch('https://park-server.onrender.com/api/updatePrices', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ carPrice: parseFloat(carPrice), bikePrice: parseFloat(bikePrice) })
